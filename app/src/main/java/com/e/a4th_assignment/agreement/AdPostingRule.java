2b8d62b19a6415c6fb62fb2ecdc9afbfd425a0c1
AnishKumarThakur <anishthakur5577@gmail.com>
2020-01-10 15:36:24 +0545

    Adapter and terms

---
+++ app/src/main/java/com/e/a4th_assignment/agreement/AdPostingRule.java
@@ -0,0 +1,28 @@
+package com.e.a4th_assignment.agreement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AdPostingRule extends AppCompatActivity {
+
+
+    WebView webViewAdPosting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_posting_rule);
+
+
+        webViewAdPosting= findViewById(R.id.webViewAdPosting);
+
+        webViewAdPosting.getSettings().setJavaScriptEnabled(true);
+        webViewAdPosting.getSettings().setAppCacheEnabled(true);
+        webViewAdPosting.setWebViewClient(new WebViewClient());
+        webViewAdPosting.loadUrl("https://hamrobazaar.com/postrules.html");
+
+    }
+}
